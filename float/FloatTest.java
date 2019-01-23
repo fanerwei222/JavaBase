919ea4b36cef4255a98e24d3ec43f01a7be12dc7
樊伟 <fanerwei222@163.com>
2019-01-23 17:55:38 +0800

    Create FloatTest.java

---
+++ float/FloatTest.java
@@ -0,0 +1,19 @@
+package main.base.floate;
+
+/**
+ * 浮点测试
+ * @author fanwei
+ *
+ */
+public class FloatTest
+{
+    public static void main(String[] args)
+    {
+        float f1 = 1.3f;
+        float f2 = 1.2f;
+        System.out.println(f1 + f2);
+        System.out.println(f1 - f2);
+        System.out.println(f1 * f2);
+        System.out.println(f1 / f2);
+    }
+}
